7dbb4c52d57b1d87e29d4be6a80cc7373309e4b8
Sven Schneider <sven.schneider@h-brs.de>
2020-08-25 17:22:25 +0200

    add simple cmake find script for kortex api
    This version uses absolute system paths for now.

---
+++ cmake/FindKortexApi.cmake
@@ -0,0 +1,14 @@
+set(KORTEXAPI_INCLUDE_DIRS
+  "/usr/include/kortex_api"
+  "/usr/include/kortex_api/client"
+  "/usr/include/kortex_api/client_stubs"
+  "/usr/include/kortex_api/common"
+  "/usr/include/kortex_api/google"
+  "/usr/include/kortex_api/messages"
+)
+
+set(KORTEXAPI_LIBRARIES
+  "/usr/lib/libKortexApiCpp.a"
+  "/usr/lib/libKortexApiCpp_without_protobuf.a"
+  pthread
+)
