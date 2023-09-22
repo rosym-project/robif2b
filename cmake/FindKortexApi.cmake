7f1ec808f4e35a9ae6411230b429e3ffad72a783
Sven Schneider <sven.schneider@h-brs.de>
2023-09-22 13:55:43 +0200

    use cmake install paths for kortex library

--- cmake/FindKortexApi.cmake
+++ cmake/FindKortexApi.cmake
@@ -1,14 +1,16 @@
+include(GNUInstallDirs)
+
 set(KORTEXAPI_INCLUDE_DIRS
-  "/usr/include/kortex_api"
-  "/usr/include/kortex_api/client"
-  "/usr/include/kortex_api/client_stubs"
-  "/usr/include/kortex_api/common"
-  "/usr/include/kortex_api/google"
-  "/usr/include/kortex_api/messages"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api/client"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api/client_stubs"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api/common"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api/google"
+  "${CMAKE_INSTALL_FULL_INCLUDEDIR}/kortex_api/messages"
 )
 
 set(KORTEXAPI_LIBRARIES
-  "/usr/lib/libKortexApiCpp.a"
-  "/usr/lib/libKortexApiCpp_without_protobuf.a"
+  "${CMAKE_INSTALL_FULL_LIBDIR}/libKortexApiCpp.a"
+  "${CMAKE_INSTALL_FULL_LIBDIR}/libKortexApiCpp_without_protobuf.a"
   pthread
 )
